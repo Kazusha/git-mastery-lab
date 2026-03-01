47cfc850bc6a9543dff775eb1d8f728fb902f2b9
Kazusha <limanmidjie@gmail.com>
2026-03-01 10:00:49 +0000

    feature : ajouter l'username route

--- src/server.js
+++ src/server.js
@@ -32,3 +32,8 @@ app.get("/health", (req , res) => {
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 })
+
+app.get("/username" , (req , res) => {
+    const user = {name: "John Doe"};
+    res.json(user);
+}) 
